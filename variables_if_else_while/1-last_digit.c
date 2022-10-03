321909bb18ed9fa1369fbad784b74bdbdeeda834
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-03 19:04:58 +0000

    grammar

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 
 	if (n < 6)
 	{
-		printf("The last digit of %d is %d dand is less than 6 and not 0", n, n % 10);
+		printf("Last digit of %d is %d dand is less than 6 and not 0", n, n % 10);
 	}
 	else if (n > 5)
 	{
-		printf("The last digit of %d is %d and is greater than 5", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5", n, n % 10);
 	}
 	else
 	{
-		printf("The last digit of %d is %d and is 0", n, n % 10);
+		printf("Last digit of %d is %d and is 0", n, n % 10);
 	}
 
 	return (0);
