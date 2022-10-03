2ac69ffb97d3d4de4dd396e25f421f74d65618b4
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-03 21:57:44 +0000

    Added printing alphabet in upper and lowercase.

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Prints alphabet in lowercase, then in uppercase, then new line.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+
+	for (letter = 97; letter < 123; letter++)
+	{
+		putchar(letter);
+	}
+
+	for (letter = 65; letter < 91; letter++)
+	{
+		putchar(letter);
+	}
+
+	putchar(10);
+
+	return (0);
+}
