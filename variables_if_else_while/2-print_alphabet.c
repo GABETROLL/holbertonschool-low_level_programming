a79bd5840ab68e006ee26f2b5046803e68b5e24d
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-03 21:49:01 +0000

    Added printing alphabet.

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in lowercase.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int letter;
+	for (letter = 97; letter < 123; letter++)
+	{
+		putchar(letter);
+	}
+	return (0);
+}
+
