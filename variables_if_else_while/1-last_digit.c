7632f87a4a03e134ac0fc3955fa72f6a9a7212de
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-03 18:32:25 +0000

    added last digit

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/** 
+ * main - Prints random number, its last digit
+ * and weather its last digit is: 0, less than 6 or greater than 5.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int last_digit = n % 10;
+	
+	if (n < 6)
+	{
+		printf("The last digit of %d is and is less than 6 and not 0", n);
+	}
+	else if (n > 5)
+	{
+		printf("The last digit of %d is and is greater than 5", n);
+	}
+	else
+	{
+		printf("The last digit of %d is and is 0", n);
+	}
+
+	return (0);
+}
+
