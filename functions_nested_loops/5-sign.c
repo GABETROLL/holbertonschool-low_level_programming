b9c326e51bee2381125d192b2235c58b78bc3d28
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-05 13:49:00 +0000

    Completed assignment 5.

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * print_sign - Prints sign of number.
+ * print_sign - Prints sign of number n and returns int below.
  * @n: integer with sign to be checked.
  * Return: 1 if number > 0, 0 if n == 0, -1 i fnumber < 0.
  */
@@ -9,14 +9,17 @@ int print_sign(int n)
 {
 	if (n < 0)
 	{
+		_putchar('-');
 		return (-1);
 	}
 	else if (n > 0)
 	{
+		_putchar('+');
 		return (1);
 	}
 	else
 	{
+		_putchar('0');
 		return (0);
 	}
 }
