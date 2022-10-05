c8b4955f58d751192bc49c0ca338a97bd49e8929
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-05 15:30:27 +0000

    Added printing to 98.

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all numbers in [n, 98].
+ * @n: start of the counting function
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int current;
+
+	if (n < 98)
+	{
+		for (int current = n; current < 98; current++)
+		{
+			printf("%d", current);
+		}
+	}
+	else if (n > 98)
+	{
+		for (int current = n; current > 98; current--)
+		{
+			printf("%d", current);
+		}
+	}
+	printf("98");
+}
+
