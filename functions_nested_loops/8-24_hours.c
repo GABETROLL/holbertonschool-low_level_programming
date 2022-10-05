1ae78608ffab235d19c571ffa11dd42fc385e557
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-05 14:30:55 +0000

    flipping output.

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,26 +13,29 @@ void jack_bauer(void)
 		int minute;
 
 		int h;
-		int m;
+		int m1;
+		int m0;
 
 		for (minute = 0; minute < 60; minute++)
 		{
-			h = hour;
-
+			h1 = hour / 10;
 			_putchar('0' + h % 10);
-			h /= 10;
+			h *= 10;
 			_putchar('0' + h % 10);
 
 			_putchar(':');
 
-			m = minute;
-
+			m = minute / 10;
 			_putchar('0' + m % 10);
-			m /= 10;
+			m *= 10;
 			_putchar('0' + m % 10);
 			/*
 			 * In order to print a digit's numbers one-by-one,
 			 * we have to use the modulo and division operators.
+			 * 
+			 * I'm dividing, then multipliying to display the digits
+			 * in the correct order, most significant first.
+			 * using mod 10 then dividing outputs digits least significant first.
 			 */
 
 			_putchar('\n');
