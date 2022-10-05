73992498aa40324c0ae6bebb7e928000645f26c8
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-05 14:06:16 +0000

    fixed modulo and whitespace.

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,15 @@
 int print_last_digit(int n)
 {
 	int answer = (n % 10);
+
+	if (answer < 0)
+	{
+		answer *= (0 - 1);
+	}
+	/*
+	 * modulo leaves negative sign for negative numbers;
+	 * we don't want a negative ascii code.
+	 */
 	_putchar('0' + answer);
 	return (answer);
 }
