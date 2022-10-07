2d67bf1969d363be9e1a7c5d0b1274c3f0a7dcc6
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-07 14:25:54 +0000

    Added printing square.

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - Prints a size * size square of '#' chars.
+ * @size: size of square.
+ * Return: void
+ */
+void print_square(int size)
+{
+	int x, y;
+
+	for (x = 0; x < size; x++)
+	{
+		for (y = 0; y < size; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
+
