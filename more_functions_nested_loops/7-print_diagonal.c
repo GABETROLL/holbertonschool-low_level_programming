85bf55b47721e11f10f4f5dda8edf949f612221d
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-07 14:18:20 +0000

    Added printing diagonally.: g

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - Prints '\' n times diagonaly.
+ * @n: number of times to print '\'
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		int space;
+
+		for (space = 0; space < i; space++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar('\\');
+	}
+
+	_putchar('\n');
+}
+
