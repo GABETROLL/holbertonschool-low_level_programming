f62449fd39d51cc7623379084740926ee2fa9778
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-11 17:10:00 +0000

    Added first pointer

--- more_functions_nested_loops/100-prime_factor.c
+++ more_functions_nested_loops/100-prime_factor.c
@@ -5,18 +5,21 @@
  * Return: 0 (Success)
  */
 int main(void)
-{ 
+{
 	long int factor;
 	long int number;
-	factor = 612852475143;
+
+	factor = 1000500;
 
 	while (factor != 1)
 	{
 		number = factor;
 		for (factor = number - 1; factor > 0; factor--)
 		{
+			printf("Factor: %ld\n", factor);
 			if (number % factor == 0)
 			{
+				printf("Found: %ld in %ld\n", factor, number);
 				break;
 			}
 		}
