4deb5d244efc4a8a3192aea1680ae62f330a226b
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-11 17:19:12 +0000

    swap pointers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps values of two ints
+ * @a: int pointer
+ * @b: int pointer
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+
+	*a = *b;
+	*b = temp;
+}
+
