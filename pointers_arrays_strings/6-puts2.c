bfd1046864e04d5e60507e19f2f9c6ab3beac9b4
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-12 14:24:22 +0000

    Added printing every even character of string.

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every even character of string
+ * @str: Pointer to string to be printed
+ * Returns: void
+ */
+void puts2(char *str)
+{
+	char *pointer = str;
+	char *last = str;
+
+	while (*last != '\0')
+	{
+		last++;
+	}
+	last--;
+
+	while (pointer < last)
+	{
+		_putchar(*pointer);
+		pointer += 2;
+	}
+}
+
