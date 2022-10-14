d32bb7d5f4f3a93234afb8692832b622e3886b74
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-14 13:49:16 +0000

    Added finding any character inside a string.

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -6,4 +6,30 @@
  * @accept: bytes to search in *s.
  * Return: pointer to byte found in s
  */
-char *_str
+char *_strpbrk(char *s, char *accept)
+{
+	int s_index;
+	int accept_index;
+
+	for (s_index = 0;; s_index++)
+	{
+		for (accept_index = 0;; accept_index++)
+		{
+			if (s[s_index] == accept[accept_index])
+			{
+				return (s + s_index);
+			}
+
+			if (s[s_index] == '\0')
+			{
+				return (0);
+			}
+
+			if (accept[accept_index] == '\0')
+			{
+				break;
+			}
+		}
+	}
+}
+
