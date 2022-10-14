1398a8b555a182dcd8dd2d72a68e1961396b12e5
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-14 13:56:00 +0000

    Excluded null characters completely.

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,6 +2,7 @@
 
 /**
  * _strpbrk - Searches for any byte from *accept inside *s.
+ * (without '\0')
  * @s: String to search for *accept bytes
  * @accept: bytes to search in *s.
  * Return: pointer to byte found in s
@@ -11,25 +12,17 @@ char *_strpbrk(char *s, char *accept)
 	int s_index;
 	int accept_index;
 
-	for (s_index = 0;; s_index++)
+	for (s_index = 0; s[s_index] != '\0'; s_index++)
 	{
-		for (accept_index = 0;; accept_index++)
+		for (accept_index = 0; accept[accept_index] != '\0'; accept_index++)
 		{
 			if (s[s_index] == accept[accept_index])
 			{
 				return (s + s_index);
 			}
-
-			if (s[s_index] == '\0')
-			{
-				return (0);
-			}
-
-			if (accept[accept_index] == '\0')
-			{
-				break;
-			}
 		}
 	}
+
+	return (0);
 }
 
