4689d5ad53b02f2c23977f5f221b842e4894481c
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-14 13:40:58 +0000

    Changed comment.

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -4,7 +4,7 @@
  * _strchr - Finds c in string pointed to by s.
  * @s: string to search for c in
  * @c: char
- * Return: pointer to c if c was found, otherwise 0.
+ * Return: pointer to c inside *s if c was found, otherwise 0.
  */
 char *_strchr(char *s, char c)
 {
---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _strpbrk - Searches for any byte from *accept inside *s.
+ * @s: String to search for *accept bytes
+ * @accept: bytes to search in *s.
+ * Return: pointer to byte found in s
+ */
+char *_str
