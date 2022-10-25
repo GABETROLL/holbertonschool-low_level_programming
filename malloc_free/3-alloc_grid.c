14651b64871cc7d7284c453111fea6d947aaac6f
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-25 13:28:18 +0000

    Fixed documentation.

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,5 +1,20 @@
 #include <stdlib.h>
 
+/**
+ * alloc_grid - Creates 2d array.
+ *
+ * Creates an array of pointers to an array of integers,
+ * then returns the pointer to the array of pointers.
+ *
+ * If any malloc call goes wrong,
+ * free all allocated memory then return NULL.
+ *
+ * @width: amount of ints per 'inner' array
+ * @height: amount of int array pointers per main array
+ *
+ * Return: pointer to matrix if all mallocs succeded,
+ * otherwise NULL.
+ */
 int **alloc_grid(int width, int height)
 {
 	int **grid;
