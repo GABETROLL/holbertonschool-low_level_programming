b57541138f292d0d4e9e19549e039188c28e6f04
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-26 13:44:43 +0000

    Added freeing grid.

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees 2d matrix of ints.
+ *
+ * @grid: pointer pointing to an array of
+ * pointers to int arrays
+ *
+ * @height: amount of int arrays
+ *
+ * Return: void
+ */
+void free_grid(int **grid, int height)
+{
+	int h = height;
+
+	do {
+		free(grid[--h]);
+	} while (h);
+
+	free(grid);
+}
+
