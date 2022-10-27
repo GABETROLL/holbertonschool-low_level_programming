e58758fa38b734f750526e8e622bcc06ad21773d
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-27 01:33:58 +0000

    Added missing for loop.

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,6 +2,43 @@
 #include <string.h>
 
 /**
+ * concat_len - Returns the length of strings
+ * 's1' and 's2' if they were concatenated together
+ *
+ * using all of 's1' without the its null byte;
+ * and the first 'n' bytes of 's2' if 'n' < strlen(s2),
+ * otherwise using strlen(s2).
+ *
+ * @s1: string pointer
+ * @s2: stirng pointer
+ * @n: number of bytes to copy from 's2',
+ * unless 'n' is bigger than 's2',
+ * in which case all bytes are copied
+ *
+ * Return: length of s1 + s2 INCLUDING NULL BYTE
+ */
+int concat_len(char *s1, char *s2, unsigned int n)
+{
+	int result;
+	unsigned int new_s2_len;
+
+	result = strlen(s1) + 1;
+
+	new_s2_len = strlen(s2);
+
+	if (n < new_s2_len)
+	{
+		result += n;
+	}
+	else
+	{
+		result += new_s2_len;
+	}
+
+	return (result);
+}
+
+/**
  * string_nconcat - Creates a new string that is 's1' and
  * the first 'n' bytes of 's2' back-to-back
  * (WITHOUT their null bytes, followed by a null byte),
@@ -22,47 +59,39 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int new_len;
 	unsigned int i;
 	unsigned int s2_i;
-	unsigned int new_s2_len = strlen(s2);
 
-	if (n < new_s2_len)
-	{
-		new_s2_len = n;
-	}
-
-	new_len = strlen(s1) + new_s2_len + 1;
-	/*
-	 * We can't include space for the null chars in 's1' or 's2',
-	 * that would cause the new string to end just like 's1'.
-	 *
-	 * Instead, just we just include one final null byte
-	 * at the end, to make both strings visible back-to-back.
-	 *
-	 * Read the contents of the
-	 * 'pointers_arrays_strings' folder if you have any
-	 * confusions.
-	 */
-
+	new_len = concat_len(s1, s2, n);
 	result = malloc(new_len);
 
 	if (result == NULL)
-	{
 		return (NULL);
-	}
 	/* Failed to allocate memory. */
 
 	for (i = 0; s1[i]; i++)
 	{
 		result[i] = s1[i];
 	}
+	/* copy 's1''s bytes */
 
 	s2_i = 0;
-	for (; i < new_len;)
+	for (; i < new_len - 1;)
 	{
+		/*
+		 * using i = new_len - 1 in this for loop
+		 * would add an extra character,
+		 * since new_len counts the null byte at the end
+		 * that isn't necessarily at that position in
+		 * s2.
+		 */
 		result[i] = s2[s2_i];
 
 		i++;
 		s2_i++;
 	}
+	/* copy 's2''s bytes */
+
+	result[new_len - 1] = '\0';
+	/* Didn't touch this index in for loop. */
 
 	return (result);
 }
