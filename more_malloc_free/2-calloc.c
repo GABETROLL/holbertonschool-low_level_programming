66bd218527722d70aa818c79fccd72dcfa711aac
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-28 14:05:32 +0000

    Added setting all bytes to 0 in new array.

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,15 +1,44 @@
 #include <stdlib.h>
 
+/**
+ * _calloc - Allocates an array that contains
+ * items of a data type who's byte size is
+ * the parameter nmemb, and that its amount
+ * of items is the parameter 'size'.
+ *
+ * @nmemb - the byte size of all items in the array
+ * @size - the amount of items in the array
+ *
+ * Return: pointer to created array
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *result;
+	int byte_size;
+	/* byte size of result array */
+	char *result;
+	/* pointer to result array */
+	int i;
+	/* looping index */
 
 	if (nmemb == 0 || size == 0)
 	{
 		return (NULL);
 	}
 
-	result = malloc(nmemb * size);
-	return (result);
+	byte_size = nmemb * size;
+
+	result = malloc(byte_size);
+	if (result == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < byte_size; i++)
+	{
+		result[i] = 0;
+	}
+	/* set all bytes to 0 */
+
+	return ((void *)result);
 }
 
