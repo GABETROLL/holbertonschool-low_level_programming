054e9ed443892f4c2b52ed8f7cbccc84982616c0
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-31 15:02:54 +0000

    Added freeing the dog.

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - Frees memory allocated by making
+ * a dog_t using the new_dog function
+ * in file 4.
+ *
+ * @d: pointer to dog that would be freed
+ *
+ * Return: nothing
+ */
+void free_dog(dog_t *d)
+{
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
+
