f95e304c2307a85555632c64233f8bc2592e16d7
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-31 14:03:12 +0000

    Added initialization function for dog struct.

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+/**
+ * init_dog - Initializes dog *d with it's name, age and owner name.
+ * @d: pointer to dog struct that will be initialized
+ * @name: pointer to name attribute of dog
+ * @age: age to write in dog's age attribute
+ * @owner: pointer to owner's name string to write in in
+ * dog's owner attribute
+ *
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
+
