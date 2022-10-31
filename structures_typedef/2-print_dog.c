84672a1fbcdc74997cca00308d72c7050425316b
GABETROLL <gabriel7icplhappy@gmail.com>
2022-10-31 14:17:21 +0000

    Added null-pointer edge cases.

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,16 +1,36 @@
+#include <stdlib.h>
 #include <stdio.h>
 #include "dog.h"
 
 /**
  * print_dog - Prints all of *d's attributes in
- * "name: value\n" format.
+ * "Attribute_name: value\n" format.
  * @d: pointer to dog structure
  * Return: nothing
  */
 void print_dog(struct dog *d)
 {
-	printf("name: %s\n", d->name);
-	printf("age: %f\n", d->age);
-	printf("owner: %s\n", d->owner);
+	if (dog != NULL)
+	{
+		if (d->name == NULL)
+		{
+			printf("(nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+
+		printf("Age: %f\n", d->age);
+
+		if (d->owner == NULL)
+		{
+			printf("(nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
 }
 
