b22bd5acf07211ebdab566df8b32097dff09bbaa
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-01 17:52:03 +0000

    Added documentation.

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "function_pointers.h"
 
+/**
+ * print_name - Uses function f to print name.
+ * @name: name
+ * @f: function that prints name
+ * Return: nothing
+ */
 void print_name(char *name, void (*f)(char *))
 {
 	if (f != NULL)
