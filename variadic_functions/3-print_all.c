46fe408cd9f6665cae4c33bc4fa9c0853411c52c
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-03 18:10:49 +0000

    Changed a lot of things...

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,56 +2,6 @@
 #include <stdio.h>
 #include <string.h>
 
-/*
- * YES, WE CAN GET THE NEXT ARGUMENT IN ANOTHER FUNCTION,
- * since the function 'va_arg' just moves a pointer
- * to the next part of its allocated memory,
- * which we're free to pass around in different functions.
- *
- * and also have to free when we're done
- */
-
-/**
- * print_char - Prints char in 'items' argument list
- * @items: va_list ready to "yield" char
- * Return: nothing
- */
-void print_char(va_list items)
-{
-	printf("%c", (char)va_arg(items, int));
-}
-
-/**
- * print_int - Prints int in 'items' argument list
- * @items: va_list ready to "yield" int
- * Return: nothing
- */
-void print_int(va_list items)
-{
-	printf("%d", va_arg(items, int));
-}
-
-/**
- * print_float - Prints float in 'items' argument list
- * @items: va_list ready to "yield" float
- * Return: nothing
- */
-void print_float(va_list items)
-{
-	printf("%f", (float)va_arg(items, double));
-}
-
-/**
- * print_string - Prints string pointed to by
- * pointer in 'items' argument list
- * @items: va_list ready to "yield" char pointer
- * Return: nothing
- */
-void print_string(va_list items)
-{
-	printf("%s", va_arg(items, char *));
-}
-
 /**
  * print_all - Prints all items in '...' as
  * types in 'format'.
@@ -74,11 +24,13 @@ void print_all(const char * const format, ...)
 {
 	va_list items;
 
-	char format_chars[] = "cifs";
-	void (*print_functions[])(va_list) = {&print_char, &print_int, &print_float, &print_string, NULL};
-
 	int format_i;
 	int format_len = strlen(format);
+
+	while (format == NULL)
+	{
+		return;
+	}
 
 	va_start(items, format);
 
@@ -86,23 +38,38 @@ void print_all(const char * const format, ...)
 	format_i = 0;
 	while (format && format_i < format_len)
 	{
-		int format_char_i = 0;
+		char *s;
 
-		while (format_chars[format_char_i])
+		/* and match it to its corresponding printing function */
+		switch (format[format_i])
 		{
-			/* and match it with its corresponding print format function. */
-			if (format_chars[format_char_i] == format[format_i])
-			{
-				print_functions[format_char_i](items);
-
-				if (format_i < format_len - 1)
-				{
-					printf(", ");
-				}
+			case 'c':
+				printf("%c", (char)va_arg(items, int));
 				break;
-			}
-			format_char_i++;
+			case 'i':
+				printf("%d", va_arg(items, int));
+				break;
+			case 'f':
+				printf("%f", (float)va_arg(items, double));
+				break;
+			case 's':
+				s = va_arg(items, char *);
+
+				if (s == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+
+				printf("%s", s);
+				break;
 		}
+
+		if (format_i < format_len - 1)
+		{
+			printf(", ");
+		}
+
 		format_i++;
 	}
 
