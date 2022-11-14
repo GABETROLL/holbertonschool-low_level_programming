8183f3dc786c4f02f7dc0f951143e5d1fc3122c7
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-14 14:13:35 +0000

    Forgot documentation.

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * list_len - measures and returns length of linked list
+ * (counting the NODES, NOT the POINTERS)
+ *
+ * @h: pointer to linked list
+ * Return: amount of nodes in '*h'
+ */
 size_t list_len(const list_t *h)
 {
 	size_t size = 0;
