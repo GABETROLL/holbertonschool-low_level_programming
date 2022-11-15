ffa791c18e9adb539524eb6940921697c5e6a6f3
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-15 15:09:19 +0000

    Added measuring length of linked list.

---
+++ more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * listint_len - Returns the amount of nodes in a linked list.
+ * (NEVER counting the null pointer)
+ * @h: pointer to linked list
+ * Return: length of linked list measured in nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t len = 0;
+	listint_t *current = *(listint_t **) &h;
+
+	while (current)
+	{
+		len ++;
+		current = current->next;
+	}
+
+	return (len);
+}
+
--- more_singly_linked_lists/README.md
+++ more_singly_linked_lists/README.md
@@ -1,4 +1,5 @@
 # More linked lists
 ## Scripts:
 0. Print all ints in a linked list of integers
+1. Print how many nodes a linked list has (NEVER counting the null pointer)
 
