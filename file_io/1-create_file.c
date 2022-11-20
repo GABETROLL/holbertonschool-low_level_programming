23244da2be4f827d88a52da3e7028e4ab0d6804e
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-20 21:01:42 +0000

    Fixed null string edge case.

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,8 @@
  *
  * If 'filename' is NULL or empty, return -1
  *
- * If text_content is empty, just create an empty file.
+ * If text_content is empty or NULL,
+ * just create an empty file and return 1.
  *
  * If anything else goes wrong, return -1.
  *
@@ -27,21 +28,22 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int text_content_len = strlen(text_content);
-	int successfully_written_bytes = 0;
 
 	if (filename == NULL || *filename == '\0')
 		return (-1);
 
 	if (text_content)
 	{
+		int text_content_len = strlen(text_content);
+		int successfully_written_bytes = 0;
+
 		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
 		successfully_written_bytes = write(fd, text_content, text_content_len);
 		close(fd);
+
+		return (1 ? successfully_written_bytes == text_content_len : -1);
 	}
 
-	if (successfully_written_bytes == text_content_len)
-		return (1);
-	return (-1);
+	return (1);
 }
 
