27dfcf6de4c86b3a82f711e3f46ad9d4f351ae4d
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-20 20:58:02 +0000

    Fixed edge cases

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -27,18 +27,21 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	int successfully_written_bytes;
+	int text_content_len = strlen(text_content);
+	int successfully_written_bytes = 0;
 
 	if (filename == NULL || *filename == '\0')
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
-
 	if (text_content)
-		successfully_written_bytes = write(fd, text_content, strlen(text_content));
+	{
+		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0600);
+		successfully_written_bytes = write(fd, text_content, text_content_len);
+		close(fd);
+	}
 
-	close(fd);
-
-	return (1 ? successfully_written_bytes : -1);
+	if (successfully_written_bytes == text_content_len)
+		return (1);
+	return (-1);
 }
 
