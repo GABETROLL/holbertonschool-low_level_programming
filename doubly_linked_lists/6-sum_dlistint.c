dd4b082c11a233f508bceb8445dab1acf2a8a96e
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-21 15:17:57 +0000

    add doc for function

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * sum_dlistint - read return description
+ *
+ * @head: pointer to head of linked list
+ *
+ * Return: sum of all ints in all nodes
+ * of linked list pointed to by head
 int sum_dlistint(dlistint_t *head)
 {
 	int result = 0;
