0c6e426f3ad1e4eba1781b16bcda1bc0fa0a393e
GABETROLL <gabriel7icplhappy@gmail.com>
2022-11-23 13:55:13 +0000

    Added creating a hash table.

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Tries to create new hash table
+ * of size 'size'.
+ *
+ * If it succeeds, it returns the new hash table.
+ * If it fails, it returns NULL.
+ *
+ * @size: size of hash table
+ * Return: hash table of size 'size' if successful,
+ * NULL otherwise
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *result = malloc(sizeof(hash_table_t));
+	if (result == NULL)
+		return (NULL);
+
+	result->size = size;
+	result->array = calloc(0, sizeof(hash_node_t *) * size);
+	if (result->array == NULL)
+		return (NULL);
+
+	return (result);
+}
+
